65aa7f625776e15a0caf7af67a8c3aa5fd3c3018
CarlosD21 <113545320+CarlosD21@users.noreply.github.com>
2025-11-12 12:20:16 +0100

    ajustes de rewards

--- scripts/robot.gd
+++ scripts/robot.gd
@@ -78,10 +78,10 @@ func _get_observations() -> Array:
 				if distance <= 0.75:
 					# contacto directo
 					if objetiveCatched == 1.0:
-						reward_local -= 8.0  # castigo por ser alcanzado con objetivo
+						reward_local -= 5.0  # castigo por ser alcanzado con objetivo
 						enemyTouchedMy = 1.0
 					elif objetivo.catched == 1.0 and objetiveCatched == 0.0:
-						reward_local += 2.0  # premio por interceptar rival con objetivo
+						reward_local += 10.0  # premio por interceptar rival con objetivo
 						win = true
 				elif objetiveCatched == 1.0:
 					reward_local -= 0.05  # pequeña penalización por estar muy cerca sin propósito
